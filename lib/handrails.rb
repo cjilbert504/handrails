744b844f7af4b51a8efd54ab3f764231d4d0379a
Collin Jilbert <cjilbert504@gmail.com>
2020-11-10 18:09:30 -0600

    version 0.0.2

--- lib/handrails.rb
+++ lib/handrails.rb
@@ -1,12 +1,27 @@
 require "handrails/version"
-require "handrails/array"
+require "handrails/routing"
 
 module Handrails
   class Application
     def call(env)
-      `echo debug > debug.txt`
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, { "Content-Type" => "text/html" }, []]
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, { "Content-Type" => "text/html" },
-      ["Hello from Ruby on Handrails!"]]
+      [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
